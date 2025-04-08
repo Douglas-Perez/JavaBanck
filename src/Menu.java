53f694b0d7e25b461ee2bd4f6e15335f40998f46
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-04-08 16:34:34 -0300

    criar classe Menu

---
+++ src/Menu.java
@@ -0,0 +1,26 @@
+public class Menu {
+    static void ExibirInicial(Usuario cliente) {
+        String mensagem = """
+                ******************************
+                Dodos iniciais do cliente:
+                
+                Nome:            %s
+                Tipo conta:      Corrente
+                Saldo inicial:   %d
+                """;
+
+        System.out.printf((mensagem) + "%n",cliente.nome,cliente.saldo);
+    }
+
+    static void ExibirEscolha() {
+        String mensagem = """
+                1- Consultar dados
+                2- Receber valor
+                3- Transferir valor
+                4- sair
+                
+                Digite a opção desejada:""";
+
+        System.out.println(mensagem);
+    }
+}
